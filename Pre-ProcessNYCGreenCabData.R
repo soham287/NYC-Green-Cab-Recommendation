5e0fc664f46f879cb763b8c4eec8c30c67cdaee6
Soham Talukdar <Soham Talukdar>
2017-02-10 01:33:10 -0500

    Initial draft of Pre-processing the data

--- Pre-ProcessNYCGreenCabData.R
+++ Pre-ProcessNYCGreenCabData.R
@@ -2,3 +2,5 @@
 #library(data.table)
 #system.time(fread('green_tripdata_2016-02.csv', header = T, sep = ','))
 data<-read.csv('https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2016-02.csv', header = T, sep = ',')
+p<-as.POSIXct(strptime(data[1,3], "%Y-%m-%d %H:%M:%S"))
+p<"2016-02-14 21:59:59"
